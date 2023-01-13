c2719dbf04f4b03550e6cb445b3ac20fa733a106
Maanit972 <maanitmalhan@gmail.com>
2023-01-13 17:40:38 -0500

    Drivetrain Finished

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,148 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.sensors.Pigeon2;
+import com.ctre.phoenix.sensors.Pigeon2.AxisDirection;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+//==============================ADD SHAFT ENCODING============================
+
+public class Drivetrain extends SubsystemBase {
+  
+  private static CANSparkMax leftDriveMotor1,
+   leftDriveMotor2,  
+   leftDriveMotor3,
+   rightDriveMotor1,
+   rightDriveMotor2,
+   rightDriveMotor3;
+
+  MotorControllerGroup leftDriveMotors,
+  rightDriveMotors;
+
+
+  private final Pigeon2 pigeon; 
+
+    DifferentialDrive differentialDrive;
+
+  /** Creates a new Drivetrain. */
+  public Drivetrain() {
+
+    pigeon = new Pigeon2(Constants.pigeonIMU);
+
+    pigeon.configMountPose(AxisDirection.NegativeY, AxisDirection.PositiveZ);
+    pigeon.setYaw(0);
+
+    leftDriveMotor1 = new CANSparkMax(Constants.leftDriveMotor1, MotorType.kBrushless);
+    leftDriveMotor2 = new CANSparkMax(Constants.leftDriveMotor2, MotorType.kBrushless);
+    leftDriveMotor3 = new CANSparkMax(Constants.leftDriveMotor3, MotorType.kBrushless);
+    rightDriveMotor1 = new CANSparkMax(Constants.rightDriveMotor1, MotorType.kBrushless);
+    rightDriveMotor2 = new CANSparkMax(Constants.rightDriveMotor2, MotorType.kBrushless);
+    rightDriveMotor3 = new CANSparkMax(Constants.rightDriveMotor3, MotorType.kBrushless);
+
+    leftDriveMotors = new MotorControllerGroup(leftDriveMotor1, leftDriveMotor2, leftDriveMotor3);
+    rightDriveMotors = new MotorControllerGroup(rightDriveMotor1, rightDriveMotor2, rightDriveMotor3);
+    
+    differentialDrive = new DifferentialDrive(leftDriveMotors, rightDriveMotors);
+
+    rightDriveMotors.setInverted(true);
+
+  }
+//Yaw is abt +Z
+  public double getYaw(){
+    return pigeon.getYaw();
+  }
+//Pitch is abt +Y
+  public double getPitch(){
+    return pigeon.getPitch();
+  }
+//Roll is abt +X
+  public double getRoll(){
+    return pigeon.getRoll();
+  }
+
+
+  public void tankDrive(double leftSpeed, double rightSpeed){
+    differentialDrive.tankDrive(leftSpeed, rightSpeed);
+}
+
+  public double getLeftDrive1Pos(){
+    return leftDriveMotor1.getEncoder().getPosition();
+  }
+
+  public double getLeftDrive2Pos(){
+    return leftDriveMotor2.getEncoder().getPosition();
+  }
+
+  public double getLeftDrive3Pos(){
+    return leftDriveMotor3.getEncoder().getPosition();
+  }
+  
+  public double getRightDrive1Pos(){
+    return rightDriveMotor1.getEncoder().getPosition();
+  }
+
+  public double getRightDrive2Pos(){
+    return rightDriveMotor2.getEncoder().getPosition();
+  }
+
+  public double getRightDrive3Pos(){
+    return rightDriveMotor3.getEncoder().getPosition();
+  }
+
+  
+  public void resetEncoders(){
+    leftDriveMotor1.getEncoder().setPosition(0);
+    leftDriveMotor2.getEncoder().setPosition(0);
+    leftDriveMotor3.getEncoder().setPosition(0);
+    rightDriveMotor1.getEncoder().setPosition(0);
+    rightDriveMotor2.getEncoder().setPosition(0);
+    rightDriveMotor3.getEncoder().setPosition(0);
+  }
+
+  public double getDistanceFeet(){
+    double avgDistanceInRotations = (leftDriveMotor1.getEncoder().getPosition() 
+      + leftDriveMotor1.getEncoder().getPosition()
+      + leftDriveMotor1.getEncoder().getPosition()
+      + rightDriveMotor1.getEncoder().getPosition()
+      + rightDriveMotor1.getEncoder().getPosition()
+      + rightDriveMotor1.getEncoder().getPosition())
+
+      / 6.0;
+
+    double avgDistanceInRotationsOfShaft = avgDistanceInRotations / 14.17;
+    return Math.PI * avgDistanceInRotationsOfShaft;
+  }
+
+  public CANSparkMax getLeftDrive1(){
+    return leftDriveMotor1;
+  }
+
+  public CANSparkMax getLeftDrive2(){
+    return leftDriveMotor2;
+  }
+
+  public CANSparkMax getLeftDrive3(){
+    return leftDriveMotor3;
+  }
+  
+  public CANSparkMax getRightDrive1(){
+    return rightDriveMotor1;
+  }
+
+  public CANSparkMax getRightDrive2(){
+    return rightDriveMotor2;
+  }
+
+  public CANSparkMax getRightDrive3(){
+    return rightDriveMotor3;
+  }
+}
