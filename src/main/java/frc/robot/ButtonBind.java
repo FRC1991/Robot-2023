b547e6d587958f67d2cd91a6cddaeea10b652438
Maanit972 <maanitmalhan@gmail.com>
2023-01-28 00:14:55 -0500

    Climbstation tuning req/ Move on to NT

--- src/main/java/frc/robot/ButtonBind.java
+++ src/main/java/frc/robot/ButtonBind.java
@@ -12,7 +12,8 @@ public class ButtonBind {
 //Controllers
     public final static CommandXboxController driverController = new CommandXboxController(0);
     public final static CommandXboxController auxController = new CommandXboxController(1);
-//Driver Binding
+//DRIVER BINDINGS
+//Driver Binding For Buttons
     public final Trigger driveAButton = driverController.a();
     public final Trigger driveBButton = driverController.b();
     public final Trigger driveXButton = driverController.x();
@@ -21,40 +22,60 @@ public class ButtonBind {
     public final Trigger driveRightBumper = driverController.rightBumper();
     public final Trigger driveStartButton = driverController.start();
 
-    
+    public boolean getDriveA(){
+        return driveAButton.getAsBoolean();
+    }
+
+    public boolean getDriveB(){
+        return driveBButton.getAsBoolean();
+    }
+
+    public boolean getDriveX(){
+        return driveXButton.getAsBoolean();
+    }
+
+    public boolean getDriveY(){
+        return driveYButton.getAsBoolean();
+    }
+
+    public boolean getDriveRightBumper(){
+        return driveRightBumper.getAsBoolean();
+    }
+
+    public boolean getDriveLeftBumper(){
+        return driveLeftBumper.getAsBoolean();
+    }
+
+    public boolean getDriveStart(){
+        return driveStartButton.getAsBoolean();
+    }
+//Driving Bindings for Joysticks
     public final Trigger driveLeftStick = driverController.leftStick();
     public final double driveLeftX = driverController.getLeftX();
+    public final double driveLeftY = driverController.getLeftY();
     public final Trigger driveRightStick = driverController.rightStick();
+    public final double driveRightX = driverController.getRightX();
+    public final double driveRightY = driverController.getRightY();
     
+
+    public double getDriveLeftY(){
+        return driveLeftY;
+    }
+
+    public double getDriveLeftX(){
+        return driveLeftX;
+    }
+
+    public double getDriveRightX(){
+        return driveRightX;
+    }
+
+    public double getDriveRightY(){
+        return driveRightY;
+    }
+//Drive Bindings for triggers
     public final double driveLeftTrigger = driverController.getLeftTriggerAxis();
     public final double driveRightTrigger = driverController.getRightTriggerAxis();
-
-    public final Trigger driveDPadDown = driverController.povDown();
-    public final Trigger driveDPadUp = driverController.povUp();
-    public final Trigger driveDPadLeft = driverController.povLeft();
-    public final Trigger driveDPadRight = driverController.povRight();
-
-//Aux Binding
-    public final Trigger auxAButton = auxController.a();
-    public final Trigger auxBButton = auxController.b();
-    public final Trigger auxXButton = auxController.x();
-    public final Trigger auxYButton = auxController.y();
-    public final Trigger auxLeftBumper = auxController.leftBumper();
-    public final Trigger auxRightBumper = auxController.rightBumper();
-    public final Trigger auxStartButton = auxController.start();
-
-    
-    public final Trigger auxLeftStick = auxController.leftStick();
-    public final Trigger auxRightStick = auxController.rightStick();
-    
-    public final Trigger auxLeftTrigger = auxController.leftTrigger();
-    public final Trigger auxRightTrigger = auxController.rightTrigger();
-
-    public final Trigger auxDPadDown = auxController.povDown();
-    public final Trigger auxDPadUp = auxController.povUp();
-    public final Trigger auxDPadLeft = auxController.povLeft();
-    public final Trigger auxDPadRight = auxController.povRight();
-
 
     public double getDriveRightTrigger(){
         return driveRightTrigger;
@@ -64,18 +85,124 @@ public class ButtonBind {
         return driveLeftTrigger;
     }
 
-    public double getDriveLeftX(){
-        return driveLeftX;
+//Drive Bindings for DPad
+    public final Trigger driveDPadDown = driverController.povDown();
+    public final Trigger driveDPadUp = driverController.povUp();
+    public final Trigger driveDPadLeft = driverController.povLeft();
+    public final Trigger driveDPadRight = driverController.povRight();
+
+   
+   public Trigger getDriveDPadDown(){
+    return driveDPadDown;
+   }
+
+   public Trigger getDriveDPadUp(){
+    return driveDPadUp;
+   }
+
+   public Trigger getDriveDPadRight(){
+    return driveDPadRight;
+   }
+
+   public Trigger getDriveDPadLeft(){
+    return driveDPadLeft;
+   }
+
+    
+
+//AUX BINDINGS
+//Aux bindings for buttons
+    public final Trigger auxAButton = auxController.a();
+    public final Trigger auxBButton = auxController.b();
+    public final Trigger auxXButton = auxController.x();
+    public final Trigger auxYButton = auxController.y();
+    public final Trigger auxLeftBumper = auxController.leftBumper();
+    public final Trigger auxRightBumper = auxController.rightBumper();
+    public final Trigger auxStartButton = auxController.start();
+    
+    public boolean getAuxA(){
+        return auxAButton.getAsBoolean();
     }
 
-    public boolean getDriveX(){
-        return driveXButton.getAsBoolean();
+    public boolean getAuxB(){
+        return auxBButton.getAsBoolean();
     }
 
-    public boolean getDriveRightBumper(){
-        return driveRightBumper.getAsBoolean();
+    public boolean getAuxX(){
+        return auxXButton.getAsBoolean();
     }
 
+    public boolean getAuxY(){
+        return auxYButton.getAsBoolean();
+    }
+
+    public boolean getAuxRightBumper(){
+        return auxRightBumper.getAsBoolean();
+    }
+
+    public boolean getAuxLeftBumper(){
+        return auxLeftBumper.getAsBoolean();
+    }
+
+    public boolean getAuxStart(){
+        return auxStartButton.getAsBoolean();
+    }
+//Aux Binding for Joysticks
+    public final Trigger auxLeftStick = auxController.leftStick();
+    public final double auxLeftX = auxController.getLeftX();
+    public final double auxLeftY = auxController.getLeftY();
+    public final Trigger auxRightStick = auxController.rightStick();
+    public final double auxRightX = auxController.getRightX();
+    public final double auxRightY = auxController.getRightY();
+    
+    public double getAuxLeftY(){
+        return auxLeftY;
+    }
+
+    public double getAuxLeftX(){
+        return auxLeftX;
+    }
+
+    public double getAuxRightX(){
+        return auxRightX;
+    }
+
+    public double getAuxRightY(){
+        return auxRightY;
+    }
+//Aux Bindings for Triggers
+    public final double auxLeftTrigger = auxController.getLeftTriggerAxis();
+    public final double auxRightTrigger = auxController.getRightTriggerAxis();
+
+    public double getAuxRightTrigger(){
+        return auxRightTrigger;
+    }
+
+    public double getAuxLeftTrigger(){
+        return auxLeftTrigger;
+    }
+//Aux Bindings for DPad
+    public final Trigger auxDPadDown = auxController.povDown();
+    public final Trigger auxDPadUp = auxController.povUp();
+    public final Trigger auxDPadLeft = auxController.povLeft();
+    public final Trigger auxDPadRight = auxController.povRight();
+
+    public Trigger getAuxDPadDown(){
+        return auxDPadDown;
+       }
+    
+       public Trigger getAuxDPadUp(){
+        return auxDPadUp;
+       }
+    
+       public Trigger getAuxDPadRight(){
+        return auxDPadRight;
+       }
+    
+       public Trigger getAuxDPadLeft(){
+        return auxDPadLeft;
+       }
+    
 
 
 
